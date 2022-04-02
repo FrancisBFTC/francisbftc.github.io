6514bcc41ba6b561b4989b6be4e80ee77d06f579
FrancisBFTC <bftcorporations@gmail.com>
2022-04-02 15:18:34 -0300

    Implementa evento de teclas na página e constante de DOM dino

--- games/jogododinossauro/src/js/main.js
+++ games/jogododinossauro/src/js/main.js
@@ -1,1 +1,17 @@
 // Scripts para o jogo do dinossauro
+
+/* Constantes do D.O.M */
+// -------------------------------------------------
+
+const dino = document.querySelector('.div-dino');
+
+// -------------------------------------------------
+
+/* Eventos do teclado */
+
+let identKeyUp = (event) => {
+	if(event.keyCode === 32)
+		console.log('Pressionou espaço');
+}
+
+document.addEventListener('keyup', identKeyUp);
