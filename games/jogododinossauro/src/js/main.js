f4dcb69fe92056935855e92b756c8a775a39c700
FrancisBFTC <bftcorporations@gmail.com>
2022-04-02 15:31:03 -0300

    Implementa salto do dino através de 2 intervals: up e down

--- games/jogododinossauro/src/js/main.js
+++ games/jogododinossauro/src/js/main.js
@@ -8,10 +8,45 @@ const dino = document.querySelector('.div-dino');
 // -------------------------------------------------
 
 /* Eventos do teclado */
+// -------------------------------------------------
 
 let identKeyUp = (event) => {
 	if(event.keyCode === 32)
-		console.log('Pressionou espaço');
+		jumpDino();
 }
 
 document.addEventListener('keyup', identKeyUp);
+
+// -------------------------------------------------
+
+
+/* Funções do jogo */
+// -------------------------------------------------
+
+function jumpDino(){
+	let position = 0;
+	
+	// dinossauro subindo
+	let upInterval = setInterval(() => {
+		if(position >= 150){
+			clearInterval(upInterval);
+
+			// dinossauro descendo
+			let downInterval = setInterval(() => {
+				if(position <= 0){
+					clearInterval(downInterval);
+				}else{
+					position -= 20;
+					dino.style.bottom = `${position}px`;
+				}
+			}, 20);
+
+		}else{
+			position += 20;
+			dino.style.bottom = `${position}px`;
+		}
+	}, 20);
+
+}
+
+// -------------------------------------------------
