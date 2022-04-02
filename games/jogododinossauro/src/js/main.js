95a4af13e3c0026cfe32e1f25baef52da804407d
FrancisBFTC <bftcorporations@gmail.com>
2022-04-02 18:28:49 -0300

    Refatora funções para surgir cactos diferentes e batimento de asas dos pássaros

--- games/jogododinossauro/src/js/main.js
+++ games/jogododinossauro/src/js/main.js
@@ -6,6 +6,14 @@
 const dino = document.querySelector('.div-dino');
 const background = document.querySelector('.div-background');
 let isJumping = false;
+let score = 0;
+let birdWind = false;
+
+let imgsCactus = [
+	'img/cactus.png',
+	'img/cactus-3.png',
+	'img/cactus-4.png'
+];
 
 // -------------------------------------------------
 
@@ -17,7 +25,7 @@ let identKeyUp = (event) => {
 		if(!isJumping) jumpDino();
 }
 
-document.addEventListener('keyup', identKeyUp);
+document.addEventListener('keydown', identKeyUp);
 
 // -------------------------------------------------
 
@@ -32,7 +40,7 @@ function jumpDino(){
 
 	// dinossauro subindo
 	let upInterval = setInterval(() => {
-		if(position >= 150){
+		if(position >= 200){
 			clearInterval(upInterval);
 
 			// dinossauro descendo
@@ -58,10 +66,29 @@ function jumpDino(){
 function createCactus(){
 	const cactus = document.createElement('div');
 	let cactusPos = 1000;
+	let ramdomTime = Math.random() * 6000;
+	let indexCactus = Math.floor(Math.random() * 3);
 
 	cactus.classList.add('div-cactus');
 	cactus.style.left = `${1000}px`;
+	cactus.style.backgroundImage = `url(${imgsCactus[indexCactus]})`;
 	background.appendChild(cactus);
+
+	score++;
+
+	let leftInterval = setInterval(() => {
+		if(cactusPos < -60){
+			clearInterval(leftInterval);
+			background.removeChild(cactus);
+		}else{
+			cactusPos -= 10;
+			cactus.style.left = `${cactusPos}px`;
+		}
+
+	}, 20);
+
+
+	setTimeout(createCactus, ramdomTime);
 }
 
 // Função que cria um passáro
@@ -69,12 +96,40 @@ function createBird(){
 	const bird = document.createElement('div');
 	let birdPosX = 1000;
 	let birdPosY = Math.floor(Math.random() * 200);
+	let ramdomTime = Math.random() * 6000;
 	if(birdPosY <= 70) birdPosY = 70;
 
 	bird.classList.add('div-bird');
 	bird.style.left = `${1000}px`;
 	bird.style.bottom = `${birdPosY}px`;
 	background.appendChild(bird);
+
+	score++;
+
+	let leftIntervalB = setInterval(() => {
+		if(birdPosX < -44){
+			clearInterval(leftIntervalB);
+			clearInterval(birdWinds);
+			background.removeChild(bird);
+		}else{
+			birdPosX -= 10;
+			bird.style.left = `${birdPosX}px`;
+			
+		}
+	}, 20);
+
+	let birdWinds = setInterval(() => {
+		if(!birdWind){
+			bird.style.background = `url(img/bird-wind-down.png)`;
+			birdWind = true;
+		}
+		else{
+			bird.style.background = `url(img/bird-wind-up.png)`;
+			birdWind = false;
+		}
+	}, 250);
+
+	setTimeout(createBird, ramdomTime);
 }
 
 // -------------------------------------------------
