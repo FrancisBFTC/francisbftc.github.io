141e171ceacaa34e76036d73de5903d1445f4745
FrancisBFTC <bftcorporations@gmail.com>
2022-04-02 16:33:29 -0300

    Implementa função pra criar Cactus e Pássaro, chamando-os

--- games/jogododinossauro/src/js/main.js
+++ games/jogododinossauro/src/js/main.js
@@ -4,6 +4,7 @@
 // -------------------------------------------------
 
 const dino = document.querySelector('.div-dino');
+const background = document.querySelector('.div-background');
 let isJumping = false;
 
 // -------------------------------------------------
@@ -24,6 +25,7 @@ document.addEventListener('keyup', identKeyUp);
 /* Funções do jogo */
 // -------------------------------------------------
 
+// Função de salto do dinossauro
 function jumpDino(){
 	let position = 0;
 	isJumping = true;
@@ -52,4 +54,35 @@ function jumpDino(){
 
 }
 
+// Função que cria um cactus
+function createCactus(){
+	const cactus = document.createElement('div');
+	let cactusPos = 1000;
+
+	cactus.classList.add('div-cactus');
+	cactus.style.left = `${1000}px`;
+	background.appendChild(cactus);
+}
+
+// Função que cria um passáro
+function createBird(){
+	const bird = document.createElement('div');
+	let birdPosX = 1000;
+	let birdPosY = Math.floor(Math.random() * 200);
+	if(birdPosY <= 70) birdPosY = 70;
+
+	bird.classList.add('div-bird');
+	bird.style.left = `${1000}px`;
+	bird.style.bottom = `${birdPosY}px`;
+	background.appendChild(bird);
+}
+
 // -------------------------------------------------
+
+
+/* Chamada das funções principais do jogo */
+// -------------------------------------------------
+createCactus();
+createBird();
+// -------------------------------------------------
+
